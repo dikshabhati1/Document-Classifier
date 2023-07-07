b333c4b1879a5ec992419daf91f4ca3ab54c03c3
Diksha Bhati <70757239+dikshabhati1@users.noreply.github.com>
2023-07-07 08:29:42 +0530

    Update DocApp.py

--- DocApp.py
+++ DocApp.py
@@ -50,9 +50,9 @@ def app():
     if uploaded_file:
         if uploaded_file.name.endswith("pdf"):
             reader = PdfReader(uploaded_file)
-	    page = reader.pages[0]
-            # extracting text from page
-            file_text = page.extract_text()
+
+	    for page in reader.pages:
+	        file_text += page.extract_text()
         elif uploaded_file.name.endswith("txt") :
             file_text = uploaded_file.read()
 
